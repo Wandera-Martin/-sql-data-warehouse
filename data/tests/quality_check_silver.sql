b234e4575cfbe02137ba122fa82e6c84a6595c52
Martin Wandera <martinwandera@gmail.com>
2025-03-19 15:55:30 +0300

    add quality check script for silver data validation

---
+++ data/tests/quality_check_silver.sql
@@ -0,0 +1,16 @@
+-- Check For nulls or duplicates in the primary key column
+-- Expectation: No Results
+SELECT cst_id, COUNT(*)
+FROM silver.crm_cust_info
+WHERE cst_id IS NULL
+GROUP BY cst_id
+HAVING COUNT(*) > 1;
+
+-- check for unwanted spaces
+SELECT cst_firstname
+FROM silver.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname);
+
+-- Data Standardization & Consistency
+SELECT DISTINCT cst_gender
+FROM silver.crm_cust_info;
