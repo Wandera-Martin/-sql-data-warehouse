7c6dc3422a23227c980367dc5364da05828d0f54
Martin Wandera <martinwandera@gmail.com>
2025-03-19 15:53:22 +0300

    spelling_update

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -20,7 +20,7 @@ CREATE TABLE bronze.crm_cust_info (
     cst_firstname VARCHAR(50),
     cst_lastname VARCHAR(50),
     cst_marital_status VARCHAR(50),
-    cst_gender VARCHAR(50),
+    cst_gndr VARCHAR(50),
     cst_create_date DATE
 );
 
